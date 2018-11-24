dc31f603c6c384cd2bc1347964f200ef3770fdea
Matt Stark <b5010228@my.shu.ac.uk>
2018-11-24 17:30:50 +0000

    added two new projects, one for each server type - each will have their own class diagram

---
+++ VotingServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VotingServer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
