e9059e30b7a482f3bf12b961a4d3aef70a61d16f
Matt <b5010228@my.shu.ac.uk>
2019-03-01 22:29:44 +0000

    minor

--- eVoting-Servers/VerifyServer/server.js
+++ eVoting-Servers/VerifyServer/server.js
@@ -9,8 +9,8 @@ const db             = require('./config/db');
 
 var mongoose = require('mongoose');
 
-
-const port = 8000;
+const port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+const ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -28,7 +28,7 @@ connection.on('error', console.error.bind(console, 'connection error:'));
 connection.once('open', function() {
     require('./app/routes')(app);
     app.listen(port, function(){
-        console.log('Server started on ' + port);
+        console.log('Server started on ' + port + ' at ' + ip_address);
     });
 });
 
